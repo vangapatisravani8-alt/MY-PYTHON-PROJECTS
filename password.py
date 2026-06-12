880b7627506b61b2a83336c3c9aacd848b7da40c
vangapatisravani8-alt <vangapatisravani8@gmail.com>
2026-06-12 15:26:42 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,6 @@
+username=input("entre a username")
+password=int(input("Enter your password"))
+if username=="sravani" and password==1234: 
+ print("your login details are valid")
+else:
+ print("invalid")
